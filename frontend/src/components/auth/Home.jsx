82d2c8cf3a0be3ccad752635b296056b96ffff35
NagaSaiMullapudi <104411803+NagaSaiMullapudi@users.noreply.github.com>
2024-03-19 23:34:36 +0530

    Sai- commit-navbar-highlight
    highlighting the navbar links when scrolled

--- frontend/src/components/auth/Home.jsx
+++ frontend/src/components/auth/Home.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+
 import './../../styles/Home.css';
 import image from './image.jpeg'; // Import the image
+import Navbar from './navbar';
 
 const Home = () => {
     return (
         <div>
-            <div className="nav-container">
-                <h1>Fixit</h1>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/">Services</a>
-                    <a href="/" className="move-right">Contact Us</a>
-                </nav>
-                <Link to="/Login"><button className="login-btn">Login</button></Link> 
-                <Link to="/Signup"><button className="signup-btn">Sign Up</button></Link> 
-            </div>
+            
+              <Navbar/>
+            
+            <section id='section1'>
             <div className="image-container">
-                <img src={image} alt="Image Description" className="background-image" />
+                <img src={image} alt=" Description" className="background-image" />
                 <div className="image-text">
                     <h1>Expert HVAC, Electrical, and Plumbing Services</h1>
                     <p>Your one-stop solution for all repair needs.</p>
@@ -28,6 +23,8 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            </section>
+            <section id='section2'>
             <div className="services-container">
                 <h2>Our Services</h2>
                 <div className="card-container">
@@ -48,6 +45,29 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            </section>
+            <section id='section3'>
+            <div className="services-container">
+                <h2>Our Services</h2>
+                <div className="card-container">
+                    {/* Card 1 */}
+                    <div className="card">
+                        <h3>Service 1</h3>
+                        <p>Description of service 1</p>
+                    </div>
+                    {/* Card 2 */}
+                    <div className="card">
+                        <h3>Service 2</h3>
+                        <p>Description of service 2</p>
+                    </div>
+                    {/* Card 3 */}
+                    <div className="card">
+                        <h3>Service 3</h3>
+                        <p>Description of service 3</p>
+                    </div>
+                </div>
+            </div>
+            </section>
         </div>
     );
 }
